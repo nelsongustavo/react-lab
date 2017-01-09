6e6c00cabb6c7841f9bd68d81fb52519c85ee31b
Nelson Gustavo <nelsongborges@gmail.com>
2017-01-09 14:53:04 -0200

    Improving gulp file and style

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # react-lab
-My first react project!
+
+# Installing
+
+1. Clone the repo
+2. `npm install -g gulp` to install Gulp globally.
+3. `npm install -g json-server` to install Gulp globally.
+4. `npm install` to resolve project dependencies.
+
+# Using
+
+1. Run `json-server --watch server/db.json` from the command line.
+2. Run `npm start` from the command line and you are good to go!
+
+The project is currently setup to transpile code under the _/src_ folder using the _/src/app.js_
+file as an entry point.
+
+Our resulting code ends up in the `public` directory.
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,64 @@
-import gulp from "gulp";
-import browserify from "browserify";
-import source from "vinyl-source-stream";
+import gulp from 'gulp';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import eslint from 'gulp-eslint';
+import exorcist from 'exorcist';
+import browserSync from 'browser-sync';
+import watchify from 'watchify';
+import babelify from 'babelify';
+import uglify from 'gulp-uglify';
+import ifElse from 'gulp-if-else';
 
-gulp.task("default", ["transpile"]);
+watchify.args.debug = true;
 
-gulp.task("transpile", () => {
+const sync = browserSync.create();
 
-  return browserify("src/comments-box.js")
-    .transform("babelify", {presets: ["es2015", "react"]})
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
+// Input file.
+watchify.args.debug = true;
+var bundler = browserify('src/app.js', watchify.args);
 
+// Babel transform
+bundler.transform(babelify.configure({
+  sourceMapRelative: 'src'
+}));
+
+// On updates recompile
+bundler.on('update', bundle);
+
+function bundle() {
+  return bundler.bundle()
+    .on('error', function(error){
+      console.error( '\nError: ', error.message, '\n');
+      this.emit('end');
+    })
+    .pipe(exorcist('public/assets/js/bundle.js.map'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(ifElse(process.env.NODE_ENV === 'production', uglify))
+    .pipe(gulp.dest('public/assets/js'));
+}
+
+gulp.task('default', ['transpile']);
+
+gulp.task('transpile', ['lint'], () => bundle());
+
+gulp.task('lint', () => {
+    return gulp.src(['src/**/*.js', 'gulpfile.babel.js'])
+      .pipe(eslint())
+      .pipe(eslint.format())
 });
 
-gulp.task("watch", ["transpile"], () => {
-  gulp.watch("src/**/*", ["transpile"]);
+gulp.task('serve', ['transpile'], () => sync.init({
+  server: 'public',
+  port: process.env.PORT || 8000,
+  host: process.env.IP || 'localhost'
+}));
+
+gulp.task('js-watch', ['transpile'], () => sync.reload());
+
+gulp.task('watch', ['serve'], () => {
+  gulp.watch('src/**/*', ['js-watch'])
+  gulp.watch('public/assets/style.css', sync.reload)
+  gulp.watch('public/index.html', sync.reload)
 });
