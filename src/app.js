668e62f29b176733479f5f691ff9bec14b60d4e5
Nelson Gustavo <nelsongborges@gmail.com>
2017-03-20 17:41:20 -0300

    Adding pages and reactRoute

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentBox from './components/comments-box'
 
-document.addEventListener("DOMContentLoaded", function() { 
-  ReactDOM.render(<CommentBox />, document.getElementById("comment-box"));
+import {Router, Route} from 'react-router';
+
+import Layout from './layout/layout';
+import BlogPage from './pages/blog';
+import PicturePage from './pages/picture';
+import VideoPage from './pages/video';
+
+const app = (
+  <Router>
+    <Route path="/" component={Layout} >
+      <Route path="blog" component={BlogPage} />
+      <Route path="picture" component={PicturePage} />
+      <Route path="video" component={VideoPage} />
+    </Route>
+  </Router>
+);
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(
+    app,
+    document.getElementById("comment-box"),
+    function() {
+      console.timeEnd('react-app')
+    }
+  );
 });
